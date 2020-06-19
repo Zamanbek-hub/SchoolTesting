011296040a2cf969cf62ee018fd43eea046c10b6
Zamanbek Turukbayev <zturukbayev888@gmail.com>
2020-06-19 19:38:29 +0600

    Testing page finished

--- school/admin.py
+++ school/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from .models import *
 
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_filter = ('variant',)
+
+
 # Register your models here.
 admin.site.register(Grade)
 admin.site.register(Subject)
 admin.site.register(Variant)
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Student)
 admin.site.register(Answer)
 admin.site.register(Teacher)
